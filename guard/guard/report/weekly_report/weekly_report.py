a72ebbb33a6d1f40c03fbb332b7941dcab45287d
Ameer Muavia <mavee.shah@hotmail.com>
2024-01-16 17:00:03 +0000

    fix: employee_name instead of employee id in report now

--- guard/guard/report/weekly_report/weekly_report.py
+++ guard/guard/report/weekly_report/weekly_report.py
@@ -21,10 +21,9 @@ def get_columns():
 			"width": 300
 		},
 		{
-			"fieldname": "employee",
+			"fieldname": "employee_name",
 			"label": "Officer Name",
-			"fieldtype": "Link",
-			"options": "Employee",
+			"fieldtype": "Data",
             "align": "left",
 			"width": 200
 		},
@@ -73,7 +72,7 @@ def get_site_data(site, filters):
         frappe.qb.from_(Roster)
         .select(
             Roster.site,
-            Roster.employee,
+            Roster.employee_name,
             Roster.attendance_date,
             Roster.shift_start_time,
             Roster.shift_end_time,
@@ -121,160 +120,3 @@ def get_data(filters):
     site_data = [item for sublist in site_data for item in sublist]
     
     return site_data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_data(filters):
-
-# 	Roster = frappe.qb.DocType("Roster")
-# 	# write a query to fetch unique site names from roster between those dates
-# 	query = (
-# 	    frappe.qb.from_(Roster)
-# 	    .select(
-# 	        Roster.site
-# 	    )
-# 	    .where(
-# 	        (Roster.docstatus == docstatus_dict.get(filters.docstatus))
-# 	        & (Roster.attendance_date >= filters.from_date)
-# 	        & (Roster.attendance_date <= filters.to_date)
-# 	    ).groupby(Roster.site)
-# 	)
-# 	# run the query and get the data in a variable
-# 	sites = query.run(as_dict=1)
-# 	# fetch roster data as before but site be site and let's say if there are two sites then we should have first site data and second site data
-# 	# return that data to the report
-# 	# Fetch roster data for each site
-# 	frappe.log_error(message=str(sites),title="sites")
-# 	site_data = []
-# 	for site in sites:
-# 		site_data.append(get_site_data(site.site, filters))
-# 		# complete the function get_site_data
-# 	# return site_data
-# 	frappe.log_error(message=str(site_data),title="sites data")
-# 	return site_data
-
-# def get_site_data(site, filters):
-#     Roster = frappe.qb.DocType("Roster")
-    
-#     # write a query to fetch data for the given site between specified dates
-#     query = (
-#         frappe.qb.from_(Roster)
-#         .select(
-# 			Roster.site,
-# 			Roster.employee,
-# 			Roster.attendance_date,
-# 			Roster.shift_start_time,
-# 			Roster.shift_end_time,
-# 			Roster.hours,
-#         )
-#         .where(
-#             (Roster.docstatus == docstatus_dict.get(filters.docstatus))
-#             & (Roster.attendance_date >= filters.from_date)
-#             & (Roster.attendance_date <= filters.to_date)
-#             & (Roster.site == site)
-#         )
-#     )
-    
-#     # run the query and get the data in a variable
-#     site_data = query.run(as_dict=1)
-#     frappe.log_error(message=str(site_data),title="site data_func")
-	
-#     return site_data
-
-# 	# query = (
-# 	# 	frappe.qb.from_(Roster)
-# 	# 	.select(
-# 	# 		Roster.site,
-# 	# 		Roster.employee,
-# 	# 		Roster.attendance_date,
-# 	# 		Roster.shift_start_time,
-# 	# 		Roster.shift_end_time,
-# 	# 		Roster.hours,
-# 	# 	)
-# 	# 	.where(
-# 	# 		(Roster.docstatus == docstatus_dict.get(filters.docstatus))
-# 	# 		& (Roster.site == filters.site)
-# 	# 		& (Roster.attendance_date >= filters.from_date)
-# 	# 		& (Roster.attendance_date <= filters.to_date)
-# 	# 	)
-# 	# )
-
-# 	# return query.run(as_dict=1)
-
-
-
-# Attendance = frappe.qb.DocType("Attendance")
-# query = (
-# 	frappe.qb.from_(Attendance)
-# 	.select(
-# 		Attendance.employee,
-# 		Extract("day", Attendance.attendance_date).as_("day_of_month"),
-# 		Attendance.status,
-# 		Attendance.shift,
-# 	)
-# 	.where(
-# 		(Attendance.docstatus == 1)
-# 		& (Attendance.company == filters.company)
-# 		& (Extract("month", Attendance.attendance_date) == filters.month)
-# 		& (Extract("year", Attendance.attendance_date) == filters.year)
-# 	)
-# )
-
-# if filters.employee:
-# 	query = query.where(Attendance.employee == filters.employee)
-# query = query.orderby(Attendance.employee, Attendance.attendance_date)
-
-# return query.run(as_dict=1)
