439d6e9b00b7cc64051fc30085332fd7de3063d0
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-03-11 18:11:04 +0000

    feat: new doctype employee timesheet

---
+++ guard/guard/doctype/employee_time_sheet/employee_time_sheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Time Sheet', {
+	// refresh: function(frm) {
+
+	// }
+});
