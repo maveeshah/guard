91e4ae73bb83fefa288e0d90aadae7fd60fe10c1
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-03-11 18:14:08 +0000

    feat: emp time sheet doctype

---
+++ guard/guard/doctype/employee_time_sheet/test_employee_time_sheet.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ameer Muavia Shah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmployeeTimeSheet(FrappeTestCase):
+	pass
---
+++ guard/guard/doctype/employee_timesheet_detail/employee_timesheet_detail.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Ameer Muavia Shah and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class EmployeeTimesheetDetail(Document):
+	pass
