964cafc4a225bd5149faae8082eb54ece01c14e8
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-03-11 18:24:31 +0000

    feat: calculation of amount from hours * rate

--- guard/guard/doctype/employee_time_sheet/employee_time_sheet.js
+++ guard/guard/doctype/employee_time_sheet/employee_time_sheet.js
@@ -6,3 +6,19 @@ frappe.ui.form.on('Employee Time Sheet', {
 
 	// }
 });
+frappe.ui.form.on('Employee Timesheet Detail', {
+	rate: function (frm, cdt, cdn) {
+		const child_row = locals[cdt][cdn];
+		if (child_row.rate && child_row.hours) {
+			frappe.model.set_value(cdt, cdn, 'amount', child_row.hours * child_row.rate);
+		}
+		frm.refresh_field('details');
+	},
+	hours: function (frm, cdt, cdn) {
+		const child_row = locals[cdt][cdn];
+		if (child_row.rate && child_row.hours) {
+			frappe.model.set_value(cdt, cdn, 'amount', child_row.hours * child_row.rate);
+		}
+		frm.refresh_field('details');
+	},
+});
